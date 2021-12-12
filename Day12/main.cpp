e9d4502a23f8b16341de8d5097c6be49eafa32be
Lukas <l.abelt@luabelt.de>
2021-12-12 10:43:27 +0100

    * Finished Day 12 challenges

--- Day12/main.cpp
+++ Day12/main.cpp
@@ -4,22 +4,121 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <queue>
+#include <chrono>
+#include <iomanip>
 
 using namespace std;
 
-void part1(){
+using NextCandidate = pair<string,vector<string>>;
+
+struct PartialPath{
+  string next;
+  vector<string> path;
+  bool oneSmallVisitedTwice;
+};
+
+bool isLargeCave(string name){
+  return name[0]>='A'&&name[0]<='Z';
+}
+
+unordered_map<string,vector<string>> parseGraph(){
   std::string line;
-  while((std::cin >> line) && !line.empty()){
-    // Read input
+  unordered_map<string,std::vector<string>> edges;
+  while(getline(std::cin, line) && !line.empty()){
+    auto p = line.find("-");
+    string beg = line.substr(0,p);
+    string end = line.substr(p+1);
+
+    if(beg!="end" && end!="start") {
+      edges[beg].push_back(end);
+    }
+    if(beg!="start" && end!="end") {
+      edges[end].push_back(beg);
+    }
   }
+  return edges;
+}
+
+void part1(){
+  auto graph = parseGraph();
+
+  int res = 0;
+  std::queue<NextCandidate> open;
+  NextCandidate s{"start",{}};
+  open.emplace(s);
+
+  while (!open.empty()){
+    NextCandidate c = open.front();
+    open.pop();
+
+
+    auto currentPath = c.second;
+    currentPath.push_back(c.first);
+
+    if(c.first == "end"){
+      res +=1;
+      /*for(auto &node : currentPath){
+        cout << node << ",";
+      }
+      cout << endl;*/
+      continue;
+    }
+
+    for (auto &next : graph[c.first]) {
+      if (isLargeCave(next)){
+        open.emplace(next,currentPath);
+      } else {
+        if(std::find(currentPath.begin(), currentPath.end(), next)==currentPath.end()){
+          open.emplace(next,currentPath);
+        }
+      }
+    }
+  }
+
+  cout << res << endl;
 }
 
 void part2(){
-  std::string line;
+  auto graph = parseGraph();
 
-  while((std::cin >> line) && !line.empty()) {
-      // Read input
+  int res = 0;
+  std::queue<PartialPath> open;
+  PartialPath s{"start",{},false};
+  open.emplace(s);
+
+  while (!open.empty()){
+    PartialPath c = open.front();
+    open.pop();
+
+
+    auto currentPath = c.path;
+    currentPath.push_back(c.next);
+
+    if(c.next == "end"){
+      res +=1;
+      /*for(auto &node : currentPath){
+        cout << node << ",";
+      }
+      cout << endl;*/
+      continue;
+    }
+
+    for (auto &next : graph[c.next]) {
+      if (isLargeCave(next)){
+        open.emplace(PartialPath{next,currentPath,c.oneSmallVisitedTwice});
+      } else {
+        if(std::find(currentPath.begin(), currentPath.end(), next)==currentPath.end()){
+          open.emplace(PartialPath{next,currentPath,c.oneSmallVisitedTwice});
+        }else if(!c.oneSmallVisitedTwice){
+          open.emplace(PartialPath{next,currentPath,true});
+        }
+      }
+    }
   }
+
+  cout << res << endl;
 }
 
 int main(int argc, char* argv[]){
