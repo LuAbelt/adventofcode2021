46188d8b370cd6b6bbbcca92cb42d784b3dcc5f0
LuAbelt <l.abelt@luabelt.de>
2021-12-18 15:53:29 +0100

    * Finished Day 17

--- Day17/main.cpp
+++ Day17/main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <valarray>
 
 using namespace std;
 
@@ -12,6 +13,27 @@ void part1(){
   while((std::cin >> line) && !line.empty()){
     // Read input
   }
+}
+
+bool simulateThrow(int64_t xVel, int64_t yVel, int xMin, int xMax, int yMin, int yMax){
+    int64_t curX = 0;
+    int64_t curY = 0;
+
+    while (true){
+        curX+= xVel;
+        curY+= yVel;
+        if(curX>=xMin && curX<=xMax && curY>=yMin && curY<=yMax){
+            return true;
+        }
+
+        if(curX>xMax || curY<yMin){
+            return false;
+        }
+        if(xVel>0){
+            xVel--;
+        }
+        yVel--;
+    }
 }
 
 void part2(){
@@ -25,19 +47,30 @@ void part2(){
     size_t yBeg = line.substr(separator).find("y=");
     size_t ySep = line.substr(separator).find("..");
 
-    int xBBMin = stoi(line.substr(xBeg+2,(xSep-xBeg-2)));
-    int xBBMax = stoi(line.substr(xSep+2,(separator-xSep-2)));
+    int xMin = stoi(line.substr(xBeg+2,(xSep-xBeg-2)));
+    int xMax = stoi(line.substr(xSep+2,(separator-xSep-2)));
 
-    int yBBMin = stoi(line.substr(separator+yBeg+2,(ySep-yBeg-2)));
-    int yBBMax = stoi(line.substr(separator+ySep+2));
+    int yMin = stoi(line.substr(separator+yBeg+2,(ySep-yBeg-2)));
+    int yMax = stoi(line.substr(separator+ySep+2));
 
-    cout << line.substr(xBeg+2,(xSep-xBeg-2))<<endl;
-    cout << line.substr(xSep+2,(separator-xSep-2))<<endl;
-    cout << line.substr(separator+yBeg+2,(ySep-yBeg-2))<<endl;
-    cout << line.substr(separator+ySep+2)<<endl;
-    //cout << line.substr(xBeg+2,(xSep-xBeg-2))<<endl;
+    int64_t xLower=sqrt(xMin);
+    int64_t xUpper=xMax;
 
-    int64_t x1,x2;
+    int64_t yLower = yMin;
+    int64_t yUpper =1000;
+
+    int64_t cnt = 0;
+
+    cout << "Simualting " << (yMax-yMin)*(xMax-xMin) << " throws." << endl;
+    for (int x = xLower; x <= xUpper; ++x) {
+        for (int y = yLower; y <= yUpper; ++y) {
+            if(simulateThrow(x,y,xMin,xMax,yMin,yMax)){
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt << endl;
 }
 
 int main(int argc, char* argv[]){
